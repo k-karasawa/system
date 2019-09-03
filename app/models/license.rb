930581baea8bdf81efee10984fdfc232f8629cd8
kk_ruby <k.karasawa@kageyama-co.jp>
2019-09-03 22:02:43 +0900

    03_yoru_ie

--- app/models/license.rb
+++ app/models/license.rb
@@ -3,7 +3,7 @@ class License < ApplicationRecord
     validates :license_name, {presence: true, uniqueness: true}
     validates :expiration, {presence: true}
     validates :surveillance, {presence: true}
-    #validates :S_recommendation, {presence: true}
-    #validates :J_recommendation, {presence: true}
+    validates :S_recommendation, {presence: true}
+    validates :J_recommendation, {presence: true}
     
 end
