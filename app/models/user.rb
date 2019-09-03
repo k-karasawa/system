5d2b33faed97ce97cccfc2cd49a920b81650aceb
kk_ruby <k.karasawa@kageyama-co.jp>
2019-09-03 22:05:49 +0900

    03_yoru_ie

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,4 @@
 class User < ApplicationRecord
 validates :user_id, {presence: true, uniqueness: true}
-validates :password, {presence: true, length:{ minimum: 6 }}
+validates :password, {presence: true, length:{ minimum: 8 }}
 end
